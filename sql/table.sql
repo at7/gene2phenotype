70ebaeb04cda196a72ca16873060d174bad2deb0
Anja Thormann <anja@ebi.ac.uk>
2015-04-27 11:34:23 +0100

    Add log tables for genomic_feature_disease and genomic_feature_disease_action

--- sql/table.sql
+++ sql/table.sql
@@ -56,6 +56,16 @@ CREATE TABLE genomic_feature_disease (
   KEY disease_idx (disease_id)
 );
 
+CREATE TABLE genomic_feature_disease_log (
+  genomic_feature_disease_id int(10) unsigned NOT NULL,
+  genomic_feature_id int(10) unsigned NOT NULL,
+  disease_id int(10) unsigned NOT NULL,
+  DDD_category_attrib set('32', '33', '34', '35', '36', '37', '38', '39') DEFAULT NULL,
+  created datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  user_id int(10) unsigned NOT NULL,
+  KEY genomic_feature_disease_idx (genomic_feature_disease_id)
+);
+
 CREATE TABLE genomic_feature_disease_action (
   genomic_feature_disease_action_id int(10) unsigned NOT NULL AUTO_INCREMENT,
   genomic_feature_disease_id int(10) unsigned NOT NULL,
@@ -64,6 +74,16 @@ CREATE TABLE genomic_feature_disease_action (
   user_id int(10) unsigned DEFAULT NULL, 
   PRIMARY KEY (genomic_feature_disease_action_id),
   KEY genomic_feature_disease_idx (genomic_feature_disease_id)
+);
+
+CREATE TABLE genomic_feature_disease_action_log (
+  genomic_feature_disease_action_id int(10) unsigned NOT NULL,
+  genomic_feature_disease_id int(10) unsigned NOT NULL,
+  allelic_requirement_attrib set('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21') DEFAULT NULL,
+  mutation_consequence_attrib set('22', '23', '24', '25', '26', '27', '28', '29', '30', '31') DEFAULT NULL,
+  created datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  user_id int(10) unsigned NOT NULL, 
+  PRIMARY KEY (genomic_feature_disease_action_id),
 );
 
 CREATE TABLE genomic_feature_disease_phenotype (
