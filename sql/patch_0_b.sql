903f6b45ec4250f141545e165d564ee6a0f7dbf6
Anja Thormann <anja@ebi.ac.uk>
2015-04-28 13:24:51 +0100

    Remove primary key constraint

--- sql/patch_0_b.sql
+++ sql/patch_0_b.sql
@@ -6,7 +6,7 @@ CREATE TABLE genomic_feature_disease_action_log (
   created timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   user_id int(10) unsigned NOT NULL,
   action varchar(128) NOT NULL,
-  PRIMARY KEY (genomic_feature_disease_action_id)
+  KEY genomic_feature_disease_action_idx (genomic_feature_disease_action_id)
 );
 
 INSERT INTO genomic_feature_disease_action_log(genomic_feature_disease_action_id, genomic_feature_disease_id, allelic_requirement_attrib, mutation_consequence_attrib, created, user_id, action ) SELECT gfda.genomic_feature_disease_action_id, gfda.genomic_feature_disease_id, gfda.allelic_requirement_attrib, gfda.mutation_consequence_attrib, CURRENT_TIMESTAMP, 4, 'create' FROM genomic_feature_disease_action gfda;
