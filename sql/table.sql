626ab9752cbd30c5a7856df747a1f7563f1f6bc1
Anja Thormann <anja@ebi.ac.uk>
2015-05-05 10:39:51 +0100

    Add publication related tables

--- sql/table.sql
+++ sql/table.sql
@@ -93,6 +93,24 @@ CREATE TABLE genomic_feature_disease_phenotype (
   KEY genomic_feature_disease_idx (genomic_feature_disease_id)
 );
 
+CREATE TABLE genomic_feature_disease_publication (
+  GFD_publication_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  genomic_feature_disease_id int(10) unsigned NOT NULL,
+  publication_id int(10) unsigned NOT NULL,
+  PRIMARY KEY (GFD_publication_id),
+  KEY genomic_feature_disease_idx (genomic_feature_disease_id)
+);
+
+CREATE TABLE GFD_publication_comment (
+  GFD_publication_comment_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  GFD_publication_id int(10) unsigned NOT NULL,
+  comment_text text DEFAULT NULL,
+  created timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  user_id int(10) unsigned NOT NULL, 
+  PRIMARY KEY (GFD_publication_comment_id),
+  KEY GFD_publication_idx (GFD_publication_id)
+);
+
 CREATE TABLE genomic_feature_organ_specificity (
   genomic_feature_id int(10) unsigned NOT NULL,
   organ_specificity_id int(10) unsigned NOT NULL,
