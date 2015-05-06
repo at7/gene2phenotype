8ada4d0463fbe8e1c31bb37f8a38b34c1b76ead8
Anja Thormann <anja@ebi.ac.uk>
2015-05-06 12:40:47 +0100

    add fetch_by_dbID method

--- modules/G2P/DBSQL/GFDPublicationCommentAdaptor.pm
+++ modules/G2P/DBSQL/GFDPublicationCommentAdaptor.pm
@@ -92,6 +92,13 @@ sub delete {
   $sth->finish();
 }
 
+sub fetch_by_dbID {
+  my $self = shift;
+  my $GFD_publication_comment_id = shift;
+  my $constraint = "WHERE GFD_publication_comment_id=$GFD_publication_comment_id"; 
+  return $self->_fetch($constraint);  
+}
+
 sub fetch_all_by_GenomicFeatureDiseasePublication {
   my $self = shift;
   my $GFD_publication = shift;
@@ -101,6 +108,24 @@ sub fetch_all_by_GenomicFeatureDiseasePublication {
   my $GFD_publication_id = $GFD_publication->dbID;
   my $constraint = "WHERE GFD_publication_id=$GFD_publication_id"; 
   return $self->_fetch_all($constraint);  
+}
+
+sub _fetch {
+  my $self = shift;
+  my $constraint = shift;
+  my @gfd_publication_comments = ();
+  my $query = 'SELECT GFD_publication_comment_id, GFD_publication_id, comment_text, created, user_id FROM GFD_publication_comment';
+  $query .= " $constraint;";
+  my $dbh = $self->{dbh};
+  my $sth = $dbh->prepare($query);
+  $sth->execute() or die 'Could not execute statement ' . $sth->errstr;
+  while (my $row = $sth->fetchrow_arrayref()) {
+    my %gfd_publication_comment;
+    @gfd_publication_comment{@columns} = @$row;
+    $gfd_publication_comment{registry} = $self->{registry};
+    push @gfd_publication_comments, G2P::GFDPublicationComment->new(\%gfd_publication_comment);
+  }
+  return $gfd_publication_comments[0];
 }
 
 sub _fetch_all {
