6499416accd0d93dd8edd179c7f98ca69f0f934c
Anja Thormann <anja@ebi.ac.uk>
2015-07-08 17:23:06 +0100

    update

--- sql/patch_0_g.sql
+++ sql/patch_0_g.sql
@@ -2,5 +2,6 @@ CREATE TABLE genomic_feature_disease_organ (
   GFD_organ_id int(10) unsigned NOT NULL AUTO_INCREMENT,
   genomic_feature_disease_id int(10) unsigned NOT NULL,
   organ_id int(10) unsigned NOT NULL,
+  PRIMARY KEY (GFD_organ_id),
   KEY genomic_feature_disease_idx (genomic_feature_disease_id)
 );
