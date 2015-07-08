a11837695b3e2ec71a990765831a06f7210fc2b0
Anja Thormann <anja@ebi.ac.uk>
2015-07-08 17:20:14 +0100

    Add auto-increment

--- sql/patch_0_g.sql
+++ sql/patch_0_g.sql
@@ -1,4 +1,5 @@
 CREATE TABLE genomic_feature_disease_organ (
+  GFD_organ_id int(10) unsigned NOT NULL AUTO_INCREMENT,
   genomic_feature_disease_id int(10) unsigned NOT NULL,
   organ_id int(10) unsigned NOT NULL,
   KEY genomic_feature_disease_idx (genomic_feature_disease_id)
