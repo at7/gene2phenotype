719e09c321e92fc8467bfd63ad3db9ff55f897c3
Anja Thormann <anja@ebi.ac.uk>
2015-07-08 14:34:27 +0100

    first commit

---
+++ modules/G2P/DBSQL/PhenotypeAdaptor.pm
@@ -0,0 +1,77 @@
+use strict;
+use warnings;
+
+package G2P::DBSQL::PhenotypeAdaptor;
+
+use G2P::DBSQL::BaseAdaptor;
+use G2P::Publication;
+our @ISA = ('G2P::DBSQL::BaseAdaptor');
+
+my @columns = qw/phenotype_id pmid title source/;
+
+sub store {
+  my $self = shift;
+  my $phenotype = shift;  
+  my $dbh = $self->{dbh};
+
+  my $sth = $dbh->prepare(q{
+    INSERT INTO phenotype (
+      stable_id,
+      name,
+      description
+    ) VALUES (?,?,?);
+  });
+  $sth->execute(
+    $phenotype->stable_id || undef,
+    $phenotype->name || undef,
+    $phenotype->description || undef,
+  );
+
+  $sth->finish();
+
+  # get dbID
+  my $dbID = $dbh->last_insert_id(undef, undef, 'phen', 'phenotype_id');
+  $phenotype->{phenotype_id} = $dbID;
+  $phenotype->{registry} = $self->{registry};
+  return $phenotype;
+}
+
+sub fetch_by_phenotype_id {
+  my $self = shift;
+  my $phenotype_id = shift
+  $self->fetch_by_dbID($phenotype_id);
+}
+
+sub fetch_by_dbID {
+  my $self = shift;
+  my $phenotype_id = shift;
+  my $constraint = "WHERE phenotype_id=$phenotype_id";
+  return $self->_fetch($constraint);
+}
+
+sub fetch_by_stable_id {
+  my $self = shift;
+  my $stable_id = shift;
+  my $constraint = "WHERE stable_id=$stable_id";
+  return $self->_fetch($constraint);
+}
+
+sub _fetch {
+  my $self = shift;
+  my $constraint = shift;
+  my @phenotypes = ();
+  my $query = 'SELECT phenotype_id, stable_id, name, description FROM phenotype';
+  $query .= " $constraint;";
+  my $dbh = $self->{dbh};
+  my $sth = $dbh->prepare($query);
+  $sth->execute() or die 'Could not execute statement: ' . $sth->errstr;
+  while (my $row = $sth->fetchrow_arrayref()) {
+    my %phenotype;
+    @phenotype{@columns} = @$row;
+    $phenotype{registry} = $self->{registry};
+    push @phenotypes, G2P::Phenotype->new(\%phenotype); 
+  }
+  return $phenotypes[0];
+}
+
+1;
