7dec77c6c170cf9d9972bc0e2e060982bf48df40
Anja Thormann <anja@ebi.ac.uk>
2015-07-15 17:00:53 +0100

    add fetch_all and update

--- modules/G2P/DBSQL/PhenotypeAdaptor.pm
+++ modules/G2P/DBSQL/PhenotypeAdaptor.pm
@@ -36,6 +36,33 @@ sub store {
   return $phenotype;
 }
 
+sub update {
+  my $self = shift;
+  my $phenotype = shift;
+  my $dbh = $self->{dbh};
+
+  if (!ref($phenotype) || !$phenotype->isa('G2P::Phenotype')) {
+    die('G2P::Phenotype arg expected');
+  }
+
+  my $sth = $dbh->prepare(q{
+    UPDATE phenotype
+      SET stable_id = ?,
+          name = ?,
+          description = ?
+      WHERE phenotype_id = ? 
+  });
+  $sth->execute(
+    $phenotype->{stable_id},
+    $phenotype->{name},
+    $phenotype->{description},
+    $phenotype->dbID
+  );
+  $sth->finish();
+
+  return $phenotype;
+}
+
 sub fetch_by_phenotype_id {
   my $self = shift;
   my $phenotype_id = shift;
@@ -56,6 +83,11 @@ sub fetch_by_stable_id {
   return $self->_fetch($constraint);
 }
 
+sub fetch_all {
+  my $self = shift;
+  return $self->_fetch_all('');
+}
+
 sub _fetch {
   my $self = shift;
   my $constraint = shift;
@@ -72,6 +104,24 @@ sub _fetch {
     push @phenotypes, G2P::Phenotype->new(\%phenotype); 
   }
   return $phenotypes[0];
+}
+
+sub _fetch_all {
+  my $self = shift;
+  my $constraint = shift;
+  my @phenotypes = ();
+  my $query = 'SELECT phenotype_id, stable_id, name, description FROM phenotype';
+  $query .= " $constraint;";
+  my $dbh = $self->{dbh};
+  my $sth = $dbh->prepare($query);
+  $sth->execute() or die 'Could not execute statement: ' . $sth->errstr;
+  while (my $row = $sth->fetchrow_arrayref()) {
+    my %phenotype;
+    @phenotype{@columns} = @$row;
+    $phenotype{registry} = $self->{registry};
+    push @phenotypes, G2P::Phenotype->new(\%phenotype); 
+  }
+  return \@phenotypes;
 }
 
 1;
