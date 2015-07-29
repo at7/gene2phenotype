f75997d2e6377780137d44a999043b6b559f3cc4
Anja Thormann <anja@ebi.ac.uk>
2015-07-29 13:41:39 +0100

    update fetch variants

--- scripts/import_variants.pl
+++ scripts/import_variants.pl
@@ -16,33 +16,31 @@ GetOptions(
   'registry_file=s',
 ) or die "Erros: Failed to parse command line arguments\n";
 
-
 my $registry = G2P::Registry->new($config->{registry_file});
 my $disease_adaptor = $registry->get_adaptor('disease');
 my $GFA = $registry->get_adaptor('genomic_feature');
 my $publication_adaptor = $registry->get_adaptor('publication');
 my $variation_adaptor = $registry->get_adaptor('variation'); 
 
+# first G2P db
 my $host = '';
 my $dbname = '';
 my $user = '';
 my $password = '';
 my $dbh = DBI->connect("DBI:mysql:host=$host;database=$dbname", $user, $password, {'RaiseError' => 1});
 
-my ($var_id, $gene_symbol, $GFD_id, $disease_name, $disease_mim, $disease_id, $pmid, $publication_id, $mutation, $consequence);
+my ($var_id, $gene_symbol, $GFD_id, $disease_name, $disease_mim, $disease_id, $publication_id, $mutation, $consequence);
 my $sth = $dbh->prepare(q{
-  select v.variation_id, gf.gene_symbol, v.genomic_feature_id , d.name, d.mim, v.disease_id, p.pmid, v.publication_id, v.mutation, v.consequence
-  from publication p, disease d, genomic_feature gf, variation v
-  where v.genomic_feature_id = gf.genomic_feature_id
-  and v.disease_id = d.disease_id
-  and v.publication_id = p.publication_id;
+  SELECT v.variation_id, gf.gene_symbol, v.genomic_feature_id , d.name, d.mim, v.disease_id, v.publication_id, v.mutation, v.consequence
+  FROM disease d, genomic_feature gf, variation v
+  WHERE v.genomic_feature_id = gf.genomic_feature_id
+  AND v.disease_id = d.disease_id;
 },  {mysql_use_result => 1});
 $sth->execute() or die $dbh->errstr;
-$sth->bind_columns(\($var_id, $gene_symbol, $GFD_id, $disease_name, $disease_mim, $disease_id, $pmid, $publication_id, $mutation, $consequence));
+$sth->bind_columns(\($var_id, $gene_symbol, $GFD_id, $disease_name, $disease_mim, $disease_id, $publication_id, $mutation, $consequence));
 
-my ($GF, $disease, $publication);
 while ($sth->fetch) {
-  
+  my ($GF, $disease, $publication);
   if ($disease_name) {
     $disease = $disease_adaptor->fetch_by_name($disease_name);
     if (!$disease) {
@@ -68,14 +66,28 @@ while ($sth->fetch) {
   } else {
     print "Cannot fetch gene $gene_symbol\n";
   }  
-
-  if ($pmid) {
-    $publication = $publication_adaptor->fetch_by_PMID($pmid);
+  
+  if ($publication_id) {
+    my $pmid;    
+    my $dbh2 = DBI->connect("DBI:mysql:host=$host;database=$dbname", $user, $password, {'RaiseError' => 1});
+    my $sth2 = $dbh2->prepare(q{
+      SELECT pmid FROM publication WHERE publication_id=?;  
+    },  {mysql_use_result => 1});
+    $sth2->execute($publication_id) or die $dbh->errstr;  
+    $sth2->bind_columns(\($pmid));
+    $sth2->fetch;
+    $sth2->finish;
+    if ($pmid) {
+      $publication = $publication_adaptor->fetch_by_PMID($pmid);
+    } else {
+      print "No PMID for publication with id $publication_id\n";
+    }
   } else {
-    print "Cannot fetch publication $publication_id\n";
+    $publication = undef;
+    print "Cannot fetch publication\n";
   }
 
-  if ($disease && $GF && $publication) {
+  if ($disease && $GF) {
     my $synonyms = get_synonyms($var_id);
     my $variation = G2P::Variation->new({
       genomic_feature_id => $GF->dbID,
@@ -91,8 +103,6 @@ while ($sth->fetch) {
   } else {
     print "Data missing\n";
   }
-
-
 }
 $sth->finish();
 
