4e7d7b274926d0b09f828053bdd08a533bec3190
Anja Thormann <anja@ebi.ac.uk>
2015-08-21 10:05:02 +0100

    add 'child IF' attrib

---
+++ sql/patch_0_j.sql
@@ -0,0 +1,2 @@
+INSERT INTO attrib(attrib_type_id, value) values(3, 'child IF');
+ALTER TABLE `genomic_feature_disease` CHANGE `DDD_category_attrib` `DDD_category_attrib` SET('31','32','33','34','35');
