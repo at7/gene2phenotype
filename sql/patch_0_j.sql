8e07d5dd42e1e907a7ae48b85ad7ad036bfd7406
Anja Thormann <anja@ebi.ac.uk>
2015-08-21 10:11:07 +0100

    update genomic_feature_disease_log

--- sql/patch_0_j.sql
+++ sql/patch_0_j.sql
@@ -1,2 +1,3 @@
 INSERT INTO attrib(attrib_type_id, value) values(3, 'child IF');
 ALTER TABLE `genomic_feature_disease` CHANGE `DDD_category_attrib` `DDD_category_attrib` SET('31','32','33','34','35');
+ALTER TABLE `genomic_feature_disease_log` CHANGE `DDD_category_attrib` `DDD_category_attrib` SET('31','32','33','34','35');
