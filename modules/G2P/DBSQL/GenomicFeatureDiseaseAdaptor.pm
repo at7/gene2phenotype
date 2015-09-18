d4aee8e4b033d2647b99aa13be5b6e57dd9a3d2a
Anja Thormann <anja@ebi.ac.uk>
2015-09-18 10:54:58 +0100

    translate panel id to panel name

--- modules/G2P/DBSQL/GenomicFeatureDiseaseAdaptor.pm
+++ modules/G2P/DBSQL/GenomicFeatureDiseaseAdaptor.pm
@@ -188,6 +188,11 @@ sub _fetch {
     if ($genomic_feature_disease{DDD_category_attrib}) {
       $genomic_feature_disease{DDD_category} = $attribute_adaptor->attrib_value_for_id($genomic_feature_disease{DDD_category_attrib});
     }
+
+    if ($genomic_feature_disease{panel}) {
+      $genomic_feature_disease{panel} = $attribute_adaptor->attrib_value_for_id($genomic_feature_disease{panel});
+    }
+
     push @genomic_feature_diseases, G2P::GenomicFeatureDisease->new(\%genomic_feature_disease);
   } 
   return $genomic_feature_diseases[0]; 
