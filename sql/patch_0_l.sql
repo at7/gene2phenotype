5ba11b91293bc589330c8d2a9247c8130df52561
Anja Thormann <anja@ebi.ac.uk>
2015-09-18 10:22:24 +0100

    update spelling

--- sql/patch_0_l.sql
+++ sql/patch_0_l.sql
@@ -1,7 +1,7 @@
 INSERT INTO attrib_type(attrib_type_id, code, name, description) values(4, 'g2p_panel', 'G2P panel', 'G2P panel');
-INSERT INTO attrib(attrib_id, attrib_type_id, value) values(36, 4, 'all');
-INSERT INTO attrib(attrib_id, attrib_type_id, value) values(37, 4, 'cardiac');
-INSERT INTO attrib(attrib_id, attrib_type_id, value) values(38, 4, 'dd');
-INSERT INTO attrib(attrib_id, attrib_type_id, value) values(39, 4, 'ear');
-INSERT INTO attrib(attrib_id, attrib_type_id, value) values(40, 4, 'eye');
-INSERT INTO attrib(attrib_id, attrib_type_id, value) values(41, 4, 'skin');
+INSERT INTO attrib(attrib_id, attrib_type_id, value) values(36, 4, 'ALL');
+INSERT INTO attrib(attrib_id, attrib_type_id, value) values(37, 4, 'Cardiac');
+INSERT INTO attrib(attrib_id, attrib_type_id, value) values(38, 4, 'DD');
+INSERT INTO attrib(attrib_id, attrib_type_id, value) values(39, 4, 'Ear');
+INSERT INTO attrib(attrib_id, attrib_type_id, value) values(40, 4, 'Eye');
+INSERT INTO attrib(attrib_id, attrib_type_id, value) values(41, 4, 'Skin');
