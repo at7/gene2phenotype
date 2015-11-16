504724e41b46fc244a23287850ac0c50878306cf
Anja Thormann <anja@ebi.ac.uk>
2015-11-16 13:48:27 +0000

    add panel_attrib

--- modules/G2P/User.pm
+++ modules/G2P/User.pm
@@ -12,7 +12,9 @@ sub new {
     user_id => $params->{user_id},
     username => $params->{username},
     email => $params->{email},
-    panel => $params->{panel}
+    panel => $params->{panel},
+    panel_attrib => $params->{panel_attrib},
+    registry => $params->{registry},
   }, $class;
   return $self;
 }
@@ -34,7 +36,35 @@ sub email {
 
 sub panel {
   my $self = shift;
+  my $panel = shift;
+  my $registry = $self->{registry};
+  my $attribute_adaptor = $registry->get_adaptor('attribute');
+  if ($panel) {
+    $self->{panel} = $panel;
+    $self->{panel_attrib} = $attribute_adaptor->attrib_id_for_value($self->{panel});
+  } else {
+    if (!$self->{panel} && $self->{panel_attrib}) {
+      $self->{panel} = $attribute_adaptor->attrib_value_for_id($self->{panel_attrib});
+    }
+  }
   return $self->{panel};
+}
+
+sub panel_attrib {
+  my $self = shift;
+  my $panel_attrib = shift;
+  my $registry = $self->{registry};
+  my $attribute_adaptor = $registry->get_adaptor('attribute');
+  if ($panel_attrib) {
+    $self->{panel_attrib} = $panel_attrib;
+    $self->{panel} = $attribute_adaptor->attrib_value_for_id($self->{panel_attrib});
+  } else {
+    if (!$self->{panel_attrib} && $self->{panel}) {
+      $self->{panel_attrib} = $attribute_adaptor->attrib_id_for_value($self->{panel});
+    }
+  }
+
+  return $self->{panel_attrib};
 }
 
 1;
