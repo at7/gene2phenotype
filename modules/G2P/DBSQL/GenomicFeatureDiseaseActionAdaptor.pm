1a34a4f93009aaa9dc5f91b091abc33e6cc70142
Anja Thormann <anja@ebi.ac.uk>
2015-11-30 17:43:08 +0000

    add fetch_all

--- modules/G2P/DBSQL/GenomicFeatureDiseaseActionAdaptor.pm
+++ modules/G2P/DBSQL/GenomicFeatureDiseaseActionAdaptor.pm
@@ -151,6 +151,11 @@ sub fetch_all_by_GenomicFeatureDisease {
   return $self->_fetch_all($constraint);
 }
 
+sub fetch_all {
+  my $self = shift;
+  return $self->_fetch_all('');
+}
+
 sub _fetch {
   my $self = shift;
   my $constraint = shift;
